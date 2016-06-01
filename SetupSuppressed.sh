e5a068ee79dc3e80745e606bea086dd01e40706a
VinzenzBildstein <vbildste@uoguelph.ca>
2016-06-01 10:13:37 -0400

    Added new script to clone suppressed files The script takes the directory where the suppressed files are to be installed as argument (defaults to "suppressed"), clones from gitlab, and creates symbolic links.

---
+++ SetupSuppressed.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# check if the user provided a directory where the suppressed files are installed, otherwise use standard 'suppressed'
+if [ -z "$1" ]; then
+	supp_dir="suppressed"
+else
+	supp_dir=$1
+fi
+
+# if the directory with the suppressed files doesn't exist, clone it
+if [ ! -d $supp_dir ]; then
+	if git clone https://gitlab.com/GRIFFINCollaboration/suppressed.git $supp_dir; then
+		echo "Successfully cloned the suppressed files!"
+	else
+		echo "Failed to clone the suppressed files. Please check that your gitlab account has the rights to access this project and that your ssh-keys are properly set up!"
+		exit;
+	fi
+fi
+
+# check that the two suppressed files exist (in case someone created the directory for the suppressed files but didn't clone)
+if [ ! -f $supp_dir/DetectionSystemGriffinSuppressed.cc || ! -f $supp_dir/DetectorConstructionSuppressed.cc ]; then
+	if git clone https://gitlab.com/GRIFFINCollaboration/suppressed.git $supp_dir; then
+		echo "Successfully cloned the suppressed files!"
+	else
+		echo "Failed to clone the suppressed files. Please check that your gitlab account has the rights to access this project, that your ssh-keys are properly set up, and that $supp_dir is empty!"
+		exit;
+	fi 
+fi
+
+# at this point the directory with the suppressed files should have been set up correctly, so we just need to set the links
+ln -sf $supp_dir/DetectionSystemGriffinSuppressed.cc src/DetectionSystemGriffinSuppressed.cc
+ln -sf $supp_dir/DetectorConstructionSuppressed.cc src/DetectorConstructionSuppressed.cc
+
+echo "The suppressed files are set up, you can now compile the simulation!"
+
+exit;
