89e6a574aff2cd2832182448d0c547231c6d82da
VinzenzBildstein <vbildste@uoguelph.ca>
2016-06-01 10:56:21 -0400

    changed clone in suppressed-script to use ssh

--- SetupSuppressed.sh
+++ SetupSuppressed.sh
@@ -9,7 +9,7 @@ fi
 
 # if the directory with the suppressed files doesn't exist, clone it
 if [ ! -d $supp_dir ]; then
-	if git clone https://gitlab.com/GRIFFINCollaboration/suppressed.git $supp_dir; then
+	if git clone ssh://git@gitlab.com/GRIFFINCollaboration/suppressed.git $supp_dir; then
 		echo "Successfully cloned the suppressed files!"
 	else
 		echo "Failed to clone the suppressed files. Please check that your gitlab account has the rights to access this project and that your ssh-keys are properly set up!"
@@ -19,7 +19,7 @@ fi
 
 # check that the two suppressed files exist (in case someone created the directory for the suppressed files but didn't clone)
 if [[ ! -f $supp_dir/DetectionSystemGriffinSuppressed.cc || ! -f $supp_dir/DetectorConstructionSuppressed.cc ]]; then
-	if git clone https://gitlab.com/GRIFFINCollaboration/suppressed.git $supp_dir; then
+	if git clone ssh://git@gitlab.com/GRIFFINCollaboration/suppressed.git $supp_dir; then
 		echo "Successfully cloned the suppressed files!"
 	else
 		echo "Failed to clone the suppressed files. Please check that your gitlab account has the rights to access this project, that your ssh-keys are properly set up, and that $supp_dir is empty!"
