896b09a65ac4c73feb81be97d54faf4334b8a243
mspencerTRIUMF <ms00593@surrey.ac.uk>
2017-06-07 13:57:02 -0700

    test

--- src/Apparatus8piVacuumChamberAuxMatShell.cc
+++ src/Apparatus8piVacuumChamberAuxMatShell.cc
@@ -10,7 +10,7 @@
 #include "G4Trd.hh"
 #include "G4LogicalVolume.hh"
 #include "G4PVPlacement.hh"
-
+//github test
 #include "G4GeometryManager.hh"
 #include "G4PhysicalVolumeStore.hh"
 #include "G4LogicalVolumeStore.hh"
