a685254fbc44a2256de3bf455da7de361a4af98e
Harris Bidaman <hbidaman@HarrisBidaman.local>
2019-10-03 10:24:56 -0400

    Added PEkin and PEdep

--- include/EventAction.hh
+++ include/EventAction.hh
@@ -48,7 +48,7 @@ class HistoManager;
 
 static const int MAXSTEPS       = 1000;
 static const int MAXHITS        = 100;
-static const int NUMSTEPVARS    = 15;
+static const int NUMSTEPVARS    = 21;
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
@@ -63,8 +63,8 @@ public:
 
 	G4int GetEventNumber() { return fEvtNb;};
 
-	void AddHitTracker(const DetectorProperties& properties, const G4int& eventNumber, const G4int& trackID, const G4int& parentID, const G4int& stepNumber, const G4int& particleType, const G4int& processType, const G4double& depEnergy, const G4ThreeVector& pos, const G4double& time, const G4int& trackerZ);
-	void AddStepTracker(const DetectorProperties& properties, const G4int& eventNumber, const G4int& trackID, const G4int& parentID, const G4int& stepNumber, const G4int& particleType, const G4int& processType, const G4double& depEnergy, const G4ThreeVector& pos, const G4double& time, const G4int& trackerZ);
+	void AddHitTracker(const DetectorProperties& properties, const G4int& eventNumber, const G4int& trackID, const G4int& parentID, const G4int& stepNumber, const G4int& particleType, const G4int& processType, const G4double& depEnergy, const G4ThreeVector& pos, const G4double& time, const G4int& trackerZ, G4int total, G4int elastic, G4int inelastic, G4int numScintPhotons, G4double lab_angle, G4double final_angle, G4double TOF, G4ThreeVector TOFPos, G4double TOFMulti, G4ThreeVector TOFPosMulti, G4double PEkin, G4double PEdep);
+	void AddStepTracker(const DetectorProperties& properties, const G4int& eventNumber, const G4int& trackID, const G4int& parentID, const G4int& stepNumber, const G4int& particleType, const G4int& processType, const G4double& depEnergy, const G4ThreeVector& pos, const G4double& time, const G4int& trackerZ, G4int total, G4int elastic, G4int inelastic, G4int numScintPhotons, G4double lab_angle, G4double final_angle, G4double TOF, G4ThreeVector TOFPos, G4double TOFMulti, G4ThreeVector TOFPosMulti, G4double PEkin, G4double PEdep);
 
 
 	// Energy deposit in detection systems
@@ -75,6 +75,49 @@ public:
 	//Lab Angle
 	G4double GetLabAngle() {return pLabAngle; };
 	void SetLabAngle(G4double angle) {pLabAngle = angle; };
+	
+	//Final Angle
+	G4double GetFinalAngle() {return fFinalAngle; };
+	void SetFinalAngle(G4double angle2) {fFinalAngle = angle2; };
+	void SetFinalMomentum(G4ThreeVector finalMomentum) {fFinalMomentum = finalMomentum; };
+	void SetInitialMomentum(G4ThreeVector initialMomentum) {fInitialMomentum = initialMomentum; };
+	G4ThreeVector GetFinalMomentum() {return fFinalMomentum;};
+	G4ThreeVector GetInitialMomentum() {return fInitialMomentum;};
+	
+
+	//Counting for Efficiencies
+	void totalCounter() {++TotalCounter;};
+	G4int GetTotalCounter() {return TotalCounter;};
+	void SetTotalCounter(int count) {TotalCounter = count;};
+	void elasticCounter() {++ElasticCounter;};
+	G4int GetElasticCounter() {return ElasticCounter;};
+	void SetElasticCounter(int count1) {ElasticCounter=count1;};
+	void inelasticCounter() {++InelasticCounter;};
+	G4int GetInelasticCounter() {return InelasticCounter;};
+	void SetInelasticCounter(int count2) {InelasticCounter=count2;};
+
+	//Getting TOF
+	void SetTOF(G4double scatterTime) {fTOF = scatterTime;};
+	G4double GetTOF() {return fTOF;};
+	void SetTOFPos(G4ThreeVector scatterPos) {fTOFPos = scatterPos;};
+	G4ThreeVector GetTOFPos() {return fTOFPos;};
+	
+	void SetTOFMulti(G4double scatterTimeMulti) {fTOFMulti = scatterTimeMulti;};
+	G4double GetTOFMulti() {return fTOFMulti;};
+	void SetTOFPosMulti(G4ThreeVector scatterPosMulti) {fTOFPosMulti = scatterPosMulti;};
+	G4ThreeVector GetTOFPosMulti() {return fTOFPosMulti;};
+
+	//Counting Scintillation photons
+	void CountOneScintPhoton() {++totScintPhotons;};
+	G4int GetTotScintPhoton() {return totScintPhotons;};
+	void SetTotScintPhoton(G4int total) {totScintPhotons = total;};
+	
+	//Edep in Plastics
+	void AddPEdep(G4double pedep) {fPEdep = fPEdep + pedep;};
+	G4double GetPEdep() {return fPEdep;};
+	void SetPEdep(G4double Pedep) {fPEdep = Pedep;};
+	G4double GetPEkin() {return fPEkin;};
+	void SetPEkin(G4double Pekin) {fPEkin = Pekin;};
 
 private:
 	RunAction*    fRunAction;
@@ -111,7 +154,23 @@ private:
 
 	//Lab Angle
 	G4double pLabAngle;
-
+	G4double fFinalAngle;
+	G4ThreeVector fFinalMomentum;
+	G4ThreeVector fInitialMomentum;
+	//TOF
+	G4double fTOF;
+	G4ThreeVector fTOFPos;
+	G4double fTOFMulti;
+	G4ThreeVector fTOFPosMulti;
+	//Counters
+	G4int TotalCounter;
+	G4int InelasticCounter;
+	G4int ElasticCounter;
+	//Optical Scintillation photon counter
+	G4int totScintPhotons;
+	//Plastic edep
+	G4double fPEdep;
+	G4double fPEkin;
 };
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
